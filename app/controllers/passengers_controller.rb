5cd0eaf402f8eb7285c3c3a6dbb1b4136b79499a
Jabir Omar <Jabiromar400@gmail.com>
2022-12-11 22:35:54 +0300

    pushed

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,6 +1,22 @@
 class PassengersController < ApplicationController
+    
     def index
         passengers = Passenger.all
         render json: passengers, except: [:created_at, :updated_at]
     end
+
+    def show
+        passenger = Passenger.find_by(id:params[:id])
+        if passenger
+            render json: passenger
+        else
+            render json: { error: "passenger not found"}, status: 404
+        end
+    end
+
+    def create
+        passenger = Passenger.create(passenger_params)
+        render json: passenger
+    end
+
 end
